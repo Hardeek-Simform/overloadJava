bfb3592677a06d5d7bf994f71f57fa563f306471
hardeek-simform <hardeek.b@simformsolutions.com>
2023-04-14 17:50:40 +0530

    formatted the structure

--- src/com/add/logic/AddOverload.java
+++ src/com/add/logic/AddOverload.java
@@ -1,13 +1,15 @@
 package com.add.logic;
 
 public class AddOverload {
-    public void add(int operand1, int operand2){
-        System.out.println("The addition of the 2 Integers is : "+ (operand1+operand2));
+    public void add(int operand1, int operand2) {
+        System.out.println("The addition of the 2 Integers is : " + (operand1 + operand2));
     }
-    public void add(float operand1, float operand2){
-        System.out.println("The addition of the 2 Float is : "+ (operand1+operand2));
+
+    public void add(float operand1, float operand2) {
+        System.out.println("The addition of the 2 Float is : " + (operand1 + operand2));
     }
-    public void add(double operand1, double operand2){
-        System.out.println("The addition of the 2 Double is : "+ (operand1+operand2));
+
+    public void add(double operand1, double operand2) {
+        System.out.println("The addition of the 2 Double is : " + (operand1 + operand2));
     }
 }
